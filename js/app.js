2821125746337f6140524242b835c11feac9b14e
Tarek Ahmed <t.ahmed@kemet.com.eg>
2025-03-10 19:02:58 +0300

    updated

---
+++ js/app.js
@@ -0,0 +1,11 @@
+﻿function ChangeHeaderBackgroundOnScroll() {
+    document.addEventListener("scroll", () => {
+        var nav = document.querySelector("nav");
+        if (window.scrollY > 0) {
+            nav.classList.add("scrolled");
+        }
+        else {
+            nav.classList.remove("scrolled");
+        }
+    })
+}
